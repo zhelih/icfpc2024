06b3de28bbcfec86e26a2a5204ce79744e2c93f2
ygrek <root@ygrek.org>
2024-07-01 05:17:22 +0100

    improve lambdaman

--- src/lambdaman.ml
+++ src/lambdaman.ml
@@ -16,30 +16,27 @@ let pee _ x = x
 type cell = Wall | Pill | Reach of int
 
 let show_dir = function D -> 'D' | R -> 'R' | L -> 'L' | U -> 'U'
+let unshow_dir = function 'D' -> D | 'R' -> R | 'L' -> L | 'U' -> U | _ -> assert false
 let inverse = function U -> D | D -> U | R -> L | L -> R
 let show_pos (x,y) = sprintf "(%d,%d)" x y
 let show_cell = function Wall -> "Wall" | Pill -> "Pill" | Reach n -> sprintf "Reach %d" n
 let show grid pos = sprintf "%s %s" (show_pos pos) (show_cell @@ get grid pos)
 
-module T = Set.Make(struct type t = (int*int) * d list let compare (a,_pa) (b,_pb) = compare a b end)
-
 let rec flood grid targets step front =
   match front with
-  | [] -> ()
+  | [] -> !targets
   | _ ->
   front |> List.map (fun (f,path) ->
     let neighs = neighbours f |> List.filter (fun (_, p) -> get grid p = Pill) |> List.map (fun (d,p) -> p, d::path) in
-    if neighs = [] then targets := T.add (f,List.rev path) !targets;
+    if neighs = [] then tuck targets (f,List.rev path);
     neighs |> List.iter (fun (p,_) -> set grid p (Reach step));
 (*     neighs |> List.map fst |> List.map (show grid) |> List.iter (printfn "step %d neigh %s" step); *)
     neighs)
   |> List.concat |> flood grid targets (step+1)
 
 let flood grid start =
-  let targets = ref T.empty in
   set grid start (Reach 0);
-  flood grid targets 1 [start,[]];
-  !targets
+  flood grid (ref []) 1 [start,[]]
 
 let grid_of_string file s =
   let l = String.split_on_char '\n' s in
@@ -59,27 +56,22 @@ let invert = List.rev_map inverse
 let tuck_or_cancel l x = l := match !l with [] -> [x] | h::t when h = inverse x -> t | _ -> x :: !l
 
 let solve p grid =
-  let targets = flood grid p |> pee (T.iter (fun (p,path) -> printfn "target %s path %d" (show grid p) (List.length path))) |> ref in
+  let paths = ref [] in
+  flood grid p
+  |> pee (List.iter (fun (p,path) -> printfn "target %s path %d" (show grid p) (List.length path)))
+  |> List.iter (fun (_,path) -> tuck paths (String.of_seq @@ Seq.map show_dir @@ List.to_seq path));
   let fullpath = ref [] in
-  let _cur = ref p in
-  while not @@ T.is_empty !targets do
-    let (_,path) as target = T.choose !targets in
-(*
-      let a = Array.of_list @@ T.elements !targets in
-      a.(Random.int (Array.length a))
-    in
-*)
-    targets := T.remove target !targets;
-    (* cancel inverse faster but doesnt' matter *)
-(*     let realpath = path |> List.dropwhile (fun x -> match !fullpath with h::t when h = inverse x -> fullpath := t; true) in *)
+  let prev_full = ref [] in
+  List.sort ~cmp:String.compare !paths
+  |> List.iter begin fun path ->
+    let path = String.to_seq path |> Seq.map unshow_dir |> List.of_seq in
     path |> List.iter (tuck_or_cancel fullpath);
-(*     path |> List.iter (fun x -> cur := next !cur x; targets := T.remove (!cur,[]) !targets); *)
-    if not @@ T.is_empty !targets then invert path |> List.iter (tuck fullpath);
-  done;
-  List.rev_map (show_dir) !fullpath |> List.to_seq |> String.of_seq
+    prev_full := !fullpath;
+    invert path |> List.iter (tuck fullpath)
+  end;
+  List.rev_map (show_dir) !prev_full |> List.to_seq |> String.of_seq
 
 let solve file =
   Random.self_init ();
   let (start,grid) = grid_of_string file @@ String.trim @@ Std.input_file file in
   solve start grid
-
